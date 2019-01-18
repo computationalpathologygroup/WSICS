d51b9baed2cc1e46c7fafbd418f18962c5fc15b9
Karel <karelgerbrands@gmail.com>
2019-01-18 18:15:52 +0100

    - Merges the Classifier with the NaiveBayesClassifier

--- WSICS/Classifier/NaiveBayesClassifier.cpp
+++ WSICS/Classifier/NaiveBayesClassifier.cpp
@@ -8,78 +8,45 @@
 namespace WSICS::ML
 {
 	NaiveBayesClassifier::NaiveBayesClassifier(const size_t bins, const float blur, const ProbabilityIntegration integration_type)
-		: n_bins(bins), blur_sigma(blur), probability_integration_type(integration_type)
+		: n_bins(bins), blur_sigma(blur), probability_integration_type(integration_type), m_is_trained_(false)
 	{
 	}
 
 	NaiveBayesClassifier::NaiveBayesClassifier(const NaiveBayesClassifier& other)
-		: Classifier(other), n_bins(other.n_bins), blur_sigma(other.blur_sigma),
-		probability_integration_type(other.probability_integration_type)
+		: Classifier(other), n_bins(other.n_bins), blur_sigma(other.blur_sigma), probability_integration_type(other.probability_integration_type),
+		m_is_trained_(other.m_is_trained_), m_classes_(other.m_classes_), m_trained_feature_names_(other.m_trained_feature_names_), m_weights_(other.m_weights_)
 	{
 	}
 
-	void NaiveBayesClassifier::TrainClassifier$(const cv::ml::TrainData& train_data)
+	const std::vector<uchar> NaiveBayesClassifier::GetClasses(void) const
 	{
-		// Verify that parameters are correct.
-		if (n_bins == 0 || blur_sigma <= 0)
-		{
-			throw std::runtime_error("The number of bins must be at least 1 or higher. The sigma must be above 0.");
-		}
-
-		cv::Mat samples = train_data.getSamples();
-		cv::Mat responses = train_data.getResponses();
-		{
-			std::set<int32_t> classes;
-			for (size_t response = 0; response < responses.rows; ++response)
-			{
-				classes.insert((int32_t)responses.at<float>(response, 0));
-			}
-
-			m_classes$.clear();
-			for (const int32_t class_label : classes)
-			{
-				m_classes$.push_back(class_label);
-			}
-		}
-
-		m_feature_classifiers_.clear();
-		std::vector<float> feature_samples(train_data.getNSamples()); // input features
-		std::vector<int32_t> feature_responses(train_data.getNSamples()); // output labels
-		for (size_t feature = 0; feature < samples.cols; ++feature)
-		{
-			// Construct vectors with the input (for this feature) and the output.
-			for (size_t sample = 0; sample < samples.rows; ++sample)
-			{
-				feature_samples[sample] = samples.at<float>(sample, feature);
-				feature_responses[sample] = static_cast<int32_t>(responses.at<float>(sample, 0));
-			}
-
-			NaiveBayesFeatureClassifier feature_classifier;
-			feature_classifier.Train(feature_samples, feature_responses, GetNrOfClasses(), n_bins, blur_sigma);
-			m_feature_classifiers_.push_back(feature_classifier);
-		}
-
-		// Set the classifier as trained.
-		m_is_trained$ = true;
+		return m_classes_;
 	}
 
-	void NaiveBayesClassifier::ClassifySamples$(const cv::Mat& input, cv::Mat& output) const
+	const std::vector<std::string>& NaiveBayesClassifier::GetFeatureNames(void) const
 	{
-		// Search for the maximum output and set the class label accordingly.
-		cv::Mat posterior_output;
-		PosteriorSamples$(input, posterior_output);
-
-		output = cv::Mat::zeros(input.rows, 1, CV_8UC1);
-		for (size_t row = 0; row < posterior_output.rows; ++row)
-		{
-			double min, max;
-			cv::minMaxLoc(output.row(row), &min, &max);
-			output.at<uchar>(row, 0) = m_classes$[static_cast<uchar>(max)];
-		}
+		return m_trained_feature_names_;
+	}
+	const std::vector<double>& NaiveBayesClassifier::GetWeights(void) const
+	{
+		return m_weights_;
 	}
 
-	void NaiveBayesClassifier::PosteriorSamples$(const cv::Mat& input, cv::Mat& output) const
+	void NaiveBayesClassifier::SetWeights(const std::vector<double>& weights)
 	{
+		m_weights_ = weights;
+	}
+
+	bool NaiveBayesClassifier::IsTrained(void) const
+	{
+		return m_is_trained_;
+	}
+
+	void NaiveBayesClassifier::Posterior(const cv::Mat& input, cv::Mat& output) const
+	{
+		// Ensures this function isn't called on an untrained classifier.
+		CheckIfTrained_();
+
 		size_t features = input.cols;
 		if (features < 1 || features != m_feature_classifiers_.size())
 		{
@@ -87,7 +54,7 @@ namespace WSICS::ML
 		}
 
 		// Aggregate the probabilities for all features
-		output = cv::Mat::zeros(input.rows, this->GetNrOfClasses(), CV_32FC1);
+		output = cv::Mat::zeros(input.rows, m_classes_.size(), CV_32FC1);
 		output += 1.0;
 
 		cv::Mat feature_output;
@@ -115,7 +82,7 @@ namespace WSICS::ML
 
 			if (norm == 0)
 			{
-				output.row(row).setTo(cv::Scalar(static_cast<float>(1) / static_cast<float>(this->GetNrOfClasses())));
+				output.row(row).setTo(cv::Scalar(static_cast<float>(1) / static_cast<float>(m_classes_.size())));
 			}
 			else
 			{
@@ -125,5 +92,107 @@ namespace WSICS::ML
 				}
 			}
 		}
+	}
+
+	void NaiveBayesClassifier::Train(const cv::ml::TrainData& train_data)
+	{
+		Train(train_data, std::vector<std::string>());
+	}
+
+	void NaiveBayesClassifier::Train(const cv::ml::TrainData& train_data, const std::vector<std::string> feature_names)
+	{
+		if (train_data.getLayout() != cv::ml::ROW_SAMPLE)
+		{
+			throw std::runtime_error("The classifier requires row based samples.");
+		}
+
+		m_trained_feature_names_.clear();
+		if (!feature_names.empty())
+		{
+			m_trained_feature_names_ = feature_names;
+		}
+		else
+		{
+			m_trained_feature_names_.clear();
+			cv::Mat sample = train_data.getSamples().row(0);
+
+			for (size_t col = 0; col < sample.cols; ++col)
+			{
+				m_trained_feature_names_.push_back(std::to_string(col));
+			}
+		}
+
+		TrainClassifier_(train_data);
+	}
+
+	void NaiveBayesClassifier::Classify(const cv::Mat& input, cv::Mat& output) const
+	{
+		// Ensures this function isn't called on an untrained classifier.
+		CheckIfTrained_();
+		
+		// Search for the maximum output and set the class label accordingly.
+		cv::Mat posterior_output;
+		PosteriorSamples$(input, posterior_output);
+
+		output = cv::Mat::zeros(input.rows, 1, CV_8UC1);
+		for (size_t row = 0; row < posterior_output.rows; ++row)
+		{
+			double min, max;
+			cv::minMaxLoc(output.row(row), &min, &max);
+			output.at<uchar>(row, 0) = m_classes_[static_cast<uchar>(max)];
+		}
+	}
+
+	void NaiveBayesClassifier::CheckIfTrained_(void) const
+	{
+		if (!IsTrained())
+		{
+			throw std::runtime_error("Classifier must be trained before it can classify samples.");
+		}
+	}
+
+	void NaiveBayesClassifier::TrainClassifier_(const cv::ml::TrainData& train_data)
+	{
+		// Verify that parameters are correct.
+		if (n_bins == 0 || blur_sigma <= 0)
+		{
+			throw std::runtime_error("The number of bins must be at least 1 or higher. The sigma must be above 0.");
+		}
+
+		cv::Mat samples = train_data.getSamples();
+		cv::Mat responses = train_data.getResponses();
+		{
+			std::set<int32_t> classes;
+			for (size_t response = 0; response < responses.rows; ++response)
+			{
+				classes.insert((int32_t)responses.at<float>(response, 0));
+			}
+
+			m_classes_.clear();
+			for (const int32_t class_label : classes)
+			{
+				m_classes_.push_back(class_label);
+			}
+		}
+
+		m_feature_classifiers_.clear();
+		std::vector<float> feature_samples(train_data.getNSamples()); // input features
+		std::vector<int32_t> feature_responses(train_data.getNSamples()); // output labels
+		for (size_t feature = 0; feature < samples.cols; ++feature)
+		{
+			// Construct vectors with the input (for this feature) and the output.
+			for (size_t sample = 0; sample < samples.rows; ++sample)
+			{
+				feature_samples[sample] = samples.at<float>(sample, feature);
+				feature_responses[sample] = static_cast<int32_t>(responses.at<float>(sample, 0));
+			}
+
+			NaiveBayesFeatureClassifier feature_classifier;
+			feature_classifier.Train(feature_samples, feature_responses, this->GetClasses().size(), n_bins, blur_sigma);
+			m_feature_classifiers_.push_back(feature_classifier);
+		}
+
+		// Set the classifier as trained.
+		m_is_trained_ = true;
 	}
 }
--- WSICS/Classifier/NaiveBayesClassifier.h
+++ WSICS/Classifier/NaiveBayesClassifier.h
@@ -1,38 +1,12 @@
-#ifndef __NaiveBayesClassifier_H__
-#define __NaiveBayesClassifier_H__
-//---------------------------------------------------------------------------
+#ifndef __WSICS_CLASSIFICATION_NAIVEBAYSECLASSIFIER__
+#define __WSICS_CLASSIFICATION_NAIVEBAYSECLASSIFIER__
+
+#include <cstdint>
+
 #include "Classifier.h"
 #include "NaiveBayesFeatureClassifier.h"
 
 
-/*! \class NaiveBayesClassifier
-
-\brief Helper class for the Naive Bayesian classifier
-
-\details
-This simple classifier, aka the Idiot Bayes Classifier, assumes that all
-features are independent. It computes a probability table for each feature
-and when classifying a sample it simply multiplies the probabilities per
-feature (and renormalizes the resulting probabilities per class).
-
-See Duda and Hart 2nd edition page 61.
-
-There are two free parameters. One free parameter is the number of bins in
-each probability table. A histogram is computed for each feature with the
-given number of bins between min and max.
-This histogram may be blurred. Another free parameter controls this.
-
-IMPLEMENTED CLASSIFICATIONS
-
-<ul>
-<li>classify</li>
-<li>posterior</li>
-</ul>
-
-\author
-
-Bram van Ginneken
-*/
 namespace WSICS::ML
 {
 	class NaiveBayesClassifier : public Classifier
@@ -40,24 +14,73 @@ namespace WSICS::ML
 		public:
 			enum ProbabilityIntegration { ADDITION, MULTIPLICATION };
 			
-			size_t					n_bins;
+			uint64_t				n_bins;
 			float					blur_sigma;
 			ProbabilityIntegration	probability_integration_type;
 
-			/////////////////
-			// Default constructor
-			NaiveBayesClassifier(const size_t bins = 100, const float blur = 3, const ProbabilityIntegration integration_type = MULTIPLICATION);
+			/// <summary>
+			/// Default constructor
+			/// </summary>
+			NaiveBayesClassifier(const uint64_t bins = 100, const float blur = 3, const ProbabilityIntegration integration_type = MULTIPLICATION);
 
-			// Copy constructor
+			/// <summary>
+			/// Copy constructor
+			/// </summary>
 			NaiveBayesClassifier(const NaiveBayesClassifier& other);
 
-		protected:
-			void TrainClassifier$(const cv::ml::TrainData& train_data);
-			void ClassifySamples$(const cv::Mat& input, cv::Mat& output) const;
-			void PosteriorSamples$(const cv::Mat& input, cv::Mat& output) const;
+			const std::vector<uchar> GetClasses(void) const;
+			const std::vector<std::string>& GetFeatureNames(void) const;
+			const std::vector<double>& GetWeights(void) const;
+			void SetWeights(const std::vector<double>& weights);
+			
+			/// <summary>
+			/// Returns whether or not the classifier has been trained.
+			/// </summary>
+			/// <returns>Whether or not the classifier has been trained.</returns>
+			bool IsTrained(void) const;
+
+			/// <summary>
+			/// Trains the classifier with the given data set.
+			/// </summary>
+			/// <param name="input">The input matrix holding the samples to classify.</param>
+			/// <param name="output">The output matrix to write the results into.</param>
+			void Train(const cv::ml::TrainData& train_data);
+			/// <summary>
+			/// Trains the classifier with the given data set.
+			/// </summary>
+			/// <param name="input">The input matrix holding the samples to classify.</param>
+			/// <param name="output">The output matrix to write the results into.</param>
+			void Train(const cv::ml::TrainData& train_data, const std::vector<std::string> feature_names);
+
+			/// <summary>
+			/// Performs a hard classification the samples.
+			/// </summary>
+			/// <param name="input">The input matrix holding the samples to classify.</param>
+			/// <param name="output">The output matrix to write the results into.</param>
+			void Classify(const cv::Mat& input, cv::Mat& output) const;
+
+			/// <summary>
+			/// Performs a soft classification of the samples, providing a posterior probability for each
+			/// possible class output. These are added together and should reach one.
+			/// The number of outputs per sample equal the amount of classes.
+			/// </summary>
+			/// <param name="input">The input matrix holding the samples to classify.</param>
+			/// <param name="output">The output matrix to write the results into.</param>
+			void Posterior(const cv::Mat& input, cv::Mat& output) const;
 
 		private:
+			bool										m_is_trained_;
+			std::vector<uchar>							m_classes_;
+			std::vector<std::string>					m_trained_feature_names_;
+			std::vector<double>							m_weights_;
 			std::vector<NaiveBayesFeatureClassifier>	m_feature_classifiers_;
+
+			/// <summary>
+			/// Checks if the classifier can be used, throws a runtime exception if it can't.
+			/// </summary>
+			void CheckIfTrained_(void) const;
+
+			void TrainClassifier_(const cv::ml::TrainData& train_data);
 	};
 }
-#endif // __NaiveBayesClassifier_H__
+#endif // __WSICS_CLASSIFICATION_NAIVEBAYSECLASSIFIER__
